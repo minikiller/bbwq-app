d3f850ea2c96459e61cd779e4a6c1144cf8807a3
minikiller <minikiller@qq.com>
2020-05-31 21:00:29 +0800

    i18n

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,102 +1,58 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <el-button @click="changeLanguage">{{$t("main.language")}}</el-button>
+    <el-button type="primary" @click="changeLangEvent">Primary</el-button>
+    <span v-text="$t('m.music')"></span>
+    <br />
+    <span>{{$t('m.findMusic')}}</span>
   </div>
 </template>
 
 <script>
+import { setup, LangStorage } from "../common/lang";
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: "Welcome to Your Vue.js App"
+    };
+  },
+  methods: {
+    changeLanguage() {
+      // console.log(this.$i18n.locale)
+      if (this.$i18n.locale === "en") {
+        setup("zh");
+      } else {
+        setup("en");
+      }
+      // LangStorage.setLang(this.$i18n.locale);
+    },
+    changeLangEvent() {
+      this.$confirm(this.$t("main.confirm"), {
+        type: "warning"
+      })
+        .then(() => {
+          if (this.$i18n.locale === "en") {
+            setup("zh");
+          } else {
+            setup("en");
+          }
+        })
+        .catch(() => {
+          // this.$message({
+          //   type: "info"
+          // });
+        });
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
--- src/main.js
+++ src/main.js
@@ -1,15 +1,24 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import i18n from "./common/lang";
+import Vue from "vue";
+import ElementUI from "element-ui";
+import locale from 'element-ui/lib/locale/lang/en'
+import "element-ui/lib/theme-chalk/index.css";
+import App from "./App";
+import router from "./router";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(ElementUI, {
+  i18n: (key, value) => i18n.t(key, value)
+})
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
+  i18n,
   router,
   components: { App },
-  template: '<App/>'
-})
+  template: "<App/>"
+});
